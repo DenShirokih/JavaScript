3887c4a71d17cdf082c3887f2ca4fb7e2d3a32f2
Denis <abcdenis95@gmail.com>
2022-04-04 20:10:32 +0300

    add

--- homework-02/js/tasc_6.js
+++ homework-02/js/tasc_6.js
@@ -1,6 +1,15 @@
-let input 
-let total = 0;
-input = prompt("Вводим число");
-input = Number(input);
-total += input;
-console.log(`общая сумма ${total}`)
+// let input;
+// let total = 0;
+// while (true) {
+// input = prompt('Введіть число');
+// if (isNaN(Number(input))) {
+//     alert('Було введено не число, спробуйте ще раз');
+//     continue;
+// }
+// if (input === null) {
+//     break;
+// }
+// input = Number(input);
+// total += input;
+// }
+// console.log(`Загальна сума чисел дорівнює ${total}`);
