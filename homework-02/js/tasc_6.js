a81c3bd2f0c2eaa133a1b49ac10286a31b9fdbe4
Denis <abcdenis95@gmail.com>
2022-04-05 14:07:57 +0300

    add 7

--- homework-02/js/tasc_6.js
+++ homework-02/js/tasc_6.js
@@ -1,18 +1,18 @@
-let input;
-let total = 0;
-const array = [];
-while (true) {
-input = prompt('Вводим аккуратно число');
-if (isNaN(Number(input))) {
-    alert('я же сказал,аккуратно,это не число');
-    continue;
-}
-if (input === null) {
-    break;
-}
-input = Number(input);
-array.push(input)
-total+=input;
-}
-console.log(`вот твоя  сумма ${total}`);
-console.log(array);
+// let input;
+// let total = 0;
+// const array = [];
+// while (true) {
+// input = prompt('Вводим аккуратно число');
+// if (isNaN(Number(input))) {
+//     alert('я же сказал,аккуратно,это не число');
+//     continue;
+// }
+// if (input === null) {
+//     break;
+// }
+// input = Number(input);
+// array.push(input)
+// total+=input;
+// }
+// console.log(`вот твоя  сумма ${total}`);
+// console.log(array);
--- homework-02/js/tasc_7.js
+++ homework-02/js/tasc_7.js
@@ -1,1 +1,26 @@
-// input = prompt('Вводим аккуратно число');
+const logins = ['Mango', 'robotGoogles', 'Poly', 'Aj4x1sBozz', 'qwerty123'];
+const login= prompt("Введите новый логин")
+
+
+const isLoginValid = function(login){
+    return login.length>=4 && login.length<=16;
+};
+
+const isLoginUnique = function(allLogins, login) {
+    return !allLogins.includes(login);
+};
+
+function addLogin(allLogins, login) {
+    if (!isLoginValid(login)) {
+        return "Ошибка! Логин должен быть от 4 до 16 символов";
+    }else if (!isLoginUnique(allLogins, login)){
+        return "Такой логин уже используется!"
+    }else{
+        logins.push(login)
+        return"Логин успешно добавлен!"
+    }
+}
+/*
+ * Вызовы функции для проверки работоспособности твоей реализации.
+ */
+console.log(addLogin(logins, login)); // 'Логин успешно добавлен!'
