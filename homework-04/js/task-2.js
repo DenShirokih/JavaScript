662dc11826aab8875b83b082a38c141db89aa38e
Denis <abcdenis95@gmail.com>
2022-04-13 13:17:36 +0300

    Update task-2.js

--- homework-04/js/task-2.js
+++ homework-04/js/task-2.js
@@ -1,23 +1,26 @@
 const inventory = {
   items: ["Knife", "Gas mask"],
   add(itemName) {
+    console.log(`Adding ${itemName} to inventory`);
+
     this.items.push(itemName);
   },
   remove(itemName) {
+    console.log(`Removing ${itemName} from inventory`);
+
     this.items = this.items.filter((item) => item !== itemName);
   },
 };
+
 const invokeInventoryAction = function (itemName, action) {
-  action(itemName);
+  console.log(`Invoking action on ${itemName}`);
+  action.call(this, itemName);
 };
-invokeInventoryAction("Medkit", inventory.add);
-// Invoking action on Medkit
-// Adding Medkit to inventory
 
-console.log(inventory.items); // ['Knife', 'Gas mask', 'Medkit']
+invokeInventoryAction.call(inventory, "Medkit", inventory.add);
 
-invokeInventoryAction("Gas mask", inventory.remove);
-// Invoking action on Gas mask
-// Removing Gas mask from inventory
+console.log(inventory.items);
 
-console.log(inventory.items); // ['Knife', 'Medkit']
+invokeInventoryAction.call(inventory, "Gas mask", inventory.remove);
+
+console.log(inventory.items);
