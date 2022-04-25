0145d685b28c1112880d4d9df6151664980bb9ec
Denis <abcdenis95@gmail.com>
2022-04-25 21:22:13 +0300

    Update task-01.js

--- homework-07/js/task-01.js
+++ homework-07/js/task-01.js
@@ -1,3 +1,10 @@
 const allcategories = document.querySelectorAll('.item');
 console.log(`В списке ${allcategories.length} категории.`);
-const 
+
+const array = [...allcategories]
+  .map(
+    categories => `Категория- ${categories.children[0].textContent}
+Количество элементов: ${categories.children[1].children.length}`,
+  )
+  .join('\n');
+console.log(array);
