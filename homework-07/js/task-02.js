14391f8eb763d5228c480590c1c0a9dcf1cb6f18
Denis <abcdenis95@gmail.com>
2022-04-26 13:58:13 +0300

    Update task-02.js

--- homework-07/js/task-02.js
+++ homework-07/js/task-02.js
@@ -6,3 +6,16 @@ const ingredients = [
   'Зелень',
   'Приправы',
 ];
+const listForAddingItems = document.querySelector(`#ingredients`);
+
+const newArray = [];
+const addListItems = array => {
+  array.forEach(element => {
+    const li = document.createElement('li');
+    li.textContent = element;
+    newArray.push(li);
+  });
+};
+
+addListItems(ingredients);
+listForAddingItems.append(...newArray);
