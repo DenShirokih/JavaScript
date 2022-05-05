61ffd8c2601505d3e328aa778c87ccbc9a219cfb
Denis <abcdenis95@gmail.com>
2022-05-05 16:26:07 +0300

    Update gallery.js

--- homework-08/js/gallery.js
+++ homework-08/js/gallery.js
@@ -8,8 +8,9 @@ const refs = {
   modalOverlay: document.querySelector(".lightbox__overlay"),
 };
 
-const newLi = galleryEl.reduce((acc, { preview, original, description }) => {
-  return (acc += `<li class="gallery__item">
+const newGallEl = galleryEl.reduce(
+  (acc, { preview, original, description }) => {
+    return (acc += `<li class="gallery__item">
   <a class="gallery__link" href="${original}" >
   <img class="gallery__image"
   src="${preview}"
@@ -18,8 +19,10 @@ const newLi = galleryEl.reduce((acc, { preview, original, description }) => {
   />
   </a>
   </li>`);
-}, "");
-refs.galleryList.innerHTML = newLi;
+  },
+  ""
+);
+refs.galleryList.innerHTML = newGallEl;
 
 const openModal = (event) => {
   if (event.target.nodeName !== "IMG") {
@@ -29,6 +32,53 @@ const openModal = (event) => {
   refs.modal.classList.add("is-open");
   refs.modalImage.src = event.target.dataset.source;
   refs.modalImage.alt = event.target.alt;
+  // window.addEventListener("keydown", escCloseModal);
+  window.addEventListener("keydown", scrollingImg);
 };
 
+const closeModal = () => {
+  refs.modal.classList.remove("is-open");
+  refs.modalImage.src = " ";
+  refs.modalImage.alt = " ";
+  // window.removeEventListener("keydown", escCloseModal);
+  window.removeEventListener("keydown", scrollingImg);
+};
+
+const overlayClick = (event) => {
+  if (event.currentTarget === event.target) {
+    closeModal();
+  }
+};
+
+// const escCloseModal = (event) => {
+//   if (event.code === "Escape") {
+//     closeModal();
+//   }
+// };
+
+const scrollingImg = (event) => {
+  let imgIndex = galleryEl.findIndex(
+    (img) => img.original === refs.modalImage.src
+  );
+  console.log(imgIndex);
+  if (event.code === "ArrowRight") {
+    if (imgIndex === galleryEl.length) {
+      imgIndex = galleryEl.length;
+    }
+    imgIndex += 1;
+  }
+  if (event.code === "ArrowLeft") {
+    if (imgIndex === 0) {
+      imgIndex = 0;
+    }
+    imgIndex -= 1;
+  }
+  if (event.code === "Escape") {
+    closeModal();
+  }
+  refs.modalImage.src = galleryEl[imgIndex].original;
+  refs.modalImage.alt = galleryEl[imgIndex].description;
+};
 refs.galleryList.addEventListener("click", openModal);
+refs.modalCloseBtn.addEventListener("click", closeModal);
+refs.modalOverlay.addEventListener("click", overlayClick);
